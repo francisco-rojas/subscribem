2a5aac474912e7413b5485fc5cdbca034a7dad83
Francisco Rojas <josefcorojas@gmail.com>
2014-11-26 06:13:23 -0600

    added failed login features and handled that in sessions controller

--- app/controllers/subscribem/account/sessions_controller.rb
+++ app/controllers/subscribem/account/sessions_controller.rb
@@ -10,6 +10,10 @@ module Subscribem
       if warden.authenticate(scope: :user)
         flash[:notice] = "You are now signed in."
         redirect_to root_path
+      else
+        @user = User.new
+        flash[:error] = "Invalid email or password."
+        render action: "new"
       end
     end
   end
